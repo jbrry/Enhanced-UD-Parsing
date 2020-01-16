b7761be39421e8ed5bd264e3953dcc984a272a89
jbar-ry <james.barry26@mail.dcu.ie>
2020-01-16 16:16:40 +0000

    visualise conllu enhanced deps

---
+++ tagging/readers/conllu_enhanced_reader.py
@@ -0,0 +1,144 @@
+from typing import Dict, Tuple, List
+import logging
+
+from overrides import overrides
+from conllu import parse_incr
+
+from allennlp.common.file_utils import cached_path
+from allennlp.data.dataset_readers.dataset_reader import DatasetReader
+from allennlp.data.fields import Field, TextField, SequenceLabelField, MetadataField
+from allennlp.data.instance import Instance
+from allennlp.data.token_indexers import SingleIdTokenIndexer, TokenIndexer
+from allennlp.data.tokenizers import Token, Tokenizer
+
+logger = logging.getLogger(__name__)
+
+
+@DatasetReader.register("universal_dependencies_enhanced")
+class UniversalDependenciesEnhancedDatasetReader(DatasetReader):
+    """
+    Reads a file in the conllu Universal Dependencies format.
+    # Parameters
+    token_indexers : ``Dict[str, TokenIndexer]``, optional (default=``{"tokens": SingleIdTokenIndexer()}``)
+        The token indexers to be applied to the words TextField.
+    use_language_specific_pos : ``bool``, optional (default = False)
+        Whether to use UD POS tags, or to use the language specific POS tags
+        provided in the conllu format.
+    tokenizer : ``Tokenizer``, optional, default = None
+        A tokenizer to use to split the text. This is useful when the tokens that you pass
+        into the model need to have some particular attribute. Typically it is not necessary.
+    """
+
+    def __init__(
+        self,
+        token_indexers: Dict[str, TokenIndexer] = None,
+        use_language_specific_pos: bool = False,
+        tokenizer: Tokenizer = None,
+        lazy: bool = False,
+    ) -> None:
+        super().__init__(lazy)
+        self._token_indexers = token_indexers or {"tokens": SingleIdTokenIndexer()}
+        self.use_language_specific_pos = use_language_specific_pos
+        self.tokenizer = tokenizer
+
+    @overrides
+    def _read(self, file_path: str):
+        # if `file_path` is a URL, redirect to the cache
+        file_path = cached_path(file_path)
+
+        with open(file_path, "r") as conllu_file:
+            logger.info("Reading UD instances from conllu dataset at: %s", file_path)
+
+            for annotation in parse_incr(conllu_file):
+                #print(annotation)
+                
+                
+                # CoNLLU annotations sometimes add back in words that have been elided
+                # in the original sentence; we remove these, as we're just predicting
+                # dependencies for the original sentence.
+                # We filter by integers here as elided words have a non-integer word id,
+                # as parsed by the conllu python library.
+                #annotation = [x for x in annotation if isinstance(x["id"], int)]
+
+                heads = [x["head"] for x in annotation]
+                tags = [x["deprel"] for x in annotation]
+                
+                words = [x["form"] for x in annotation]
+                #if self.use_language_specific_pos:
+                #    pos_tags = [x["xpostag"] for x in annotation]
+                #else:
+                #    pos_tags = [x["upostag"] for x in annotation]
+                #yield self.text_to_instance(words, pos_tags, list(zip(tags, heads)))
+                deps = [x["deps"] for x in annotation]
+
+                #print(words, heads, tags, deps)
+                print("next example:")
+                print("=" * 15)
+                print("words: {}".format(words))
+                print("tags: {}".format(tags))
+                print("heads: {}".format(heads))
+                print("deps: {}:".format(deps))
+                print("=" * 15)
+                print("\n")
+
+
+
+		"""
+		next example:
+		===============
+		words: ['The', 'team', 'who', 'work', 'there', 'are', 'helpfull', ',', 'friendly', 'and', 'extremely', 'knowledgeable', 'and', 'will', 'help', 'you', 'as', 'much', 'as', 'they', 'can', 'with', 'thier', 'years', 'of', 'hands', 'on', 'practice', '.']
+		tags: ['det', 'nsubj', 'nsubj', 'acl:relcl', 'advmod', 'cop', 'root', 'punct', 'conj', 'cc', 'advmod', 'conj', 'cc', 'aux', 'conj', 'obj', 'advmod', 'advmod', 'mark', 'nsubj', 'advcl', 'case', 'nmod:poss', 'obl', 'case', 'compound', 'compound', 'nmod', 'punct']
+		heads: [2, 7, 4, 2, 4, 7, 0, 9, 7, 12, 12, 7, 15, 15, 7, 15, 18, 15, 21, 21, 17, 24, 24, 15, 28, 28, 26, 24, 7]
+		deps: [[('det', 2)], [('nsubj', 4), ('nsubj', 7), ('nsubj', 9), ('nsubj', 12), ('nsubj', 15)], [('ref', 2)], [('acl:relcl', 2)], [('advmod', 4)], [('cop', 7)], [('root', 0)], [('punct', 9)], [('conj:and', 7)], [('cc', 12)], [('advmod', 12)], [('conj:and', 7)], [('cc', 15)], [('aux', 15)], [('conj:and', 7)], [('obj', 15)], [('advmod', 18)], [('advmod', 15)], [('mark', 21)], [('nsubj', 21)], [('advcl:as', 17)], [('case', 24)], [('nmod:poss', 24)], [('obl:with', 15)], [('case', 28)], [('compound', 28)], [('compound', 26)], [('nmod:of', 24)], [('punct', 7)]]:
+		===============
+
+
+"""
+    @overrides
+    def text_to_instance(
+        self,  # type: ignore
+        words: List[str],
+        upos_tags: List[str],
+        dependencies: List[Tuple[str, int]] = None,
+    ) -> Instance:
+
+        """
+        # Parameters
+        words : ``List[str]``, required.
+            The words in the sentence to be encoded.
+        upos_tags : ``List[str]``, required.
+            The universal dependencies POS tags for each word.
+        dependencies : ``List[Tuple[str, int]]``, optional (default = None)
+            A list of  (head tag, head index) tuples. Indices are 1 indexed,
+            meaning an index of 0 corresponds to that word being the root of
+            the dependency tree.
+        # Returns
+        An instance containing words, upos tags, dependency head tags and head
+        indices as fields.
+        """
+        fields: Dict[str, Field] = {}
+
+        if self.tokenizer is not None:
+            tokens = self.tokenizer.tokenize(" ".join(words))
+        else:
+            tokens = [Token(t) for t in words]
+
+        text_field = TextField(tokens, self._token_indexers)
+        fields["words"] = text_field
+        fields["pos_tags"] = SequenceLabelField(upos_tags, text_field, label_namespace="pos")
+        if dependencies is not None:
+            # We don't want to expand the label namespace with an additional dummy token, so we'll
+            # always give the 'ROOT_HEAD' token a label of 'root'.
+            fields["head_tags"] = SequenceLabelField(
+                [x[0] for x in dependencies], text_field, label_namespace="head_tags"
+            )
+            fields["head_indices"] = SequenceLabelField(
+                [x[1] for x in dependencies], text_field, label_namespace="head_index_tags"
+            )
+
+        fields["metadata"] = MetadataField({"words": words, "pos": upos_tags})
+        return Instance(fields)
+
+
+
+
