458fe232d05dac5ff677511677afbe5f783b1cd4
jbar-ry <james.barry26@mail.dcu.ie>
2020-02-25 18:19:56 +0000

    add file to compute f1 and lf1 for enhanced labels

--- tagging/training/enhanced_attachment_scores.py
+++ tagging/training/enhanced_attachment_scores.py
@@ -9,11 +9,16 @@ from allennlp.training.metrics.metric import Metric
 @Metric.register("enhanced_attachment_scores")
 class EnhancedAttachmentScores(Metric):
     """
-    Computes labeled and unlabeled attachment scores for a
-    dependency parse, as well as sentence level exact match
-    for both labeled and unlabeled trees. Note that the input
-    to this metric is the sampled predictions, not the distribution
-    itself.
+    Computes unlabeled precision, recall and f1.
+    As the parser uses a sigmoid, initially it will predict numerous edges 
+    therefore making recall high but precision low.
+    
+    :precision: correct / system_total
+    :recall: correct / gold_total
+    :f1: 2 * correct / (system_total + gold_total)
+    
+    TODO: need to figure out how to mask punctuation which is ignored.
+    
     # Parameters
     ignore_classes : `List[int]`, optional (default = None)
         A list of label ids to ignore when computing metrics.
@@ -22,10 +27,10 @@ class EnhancedAttachmentScores(Metric):
     def __init__(self, ignore_classes: List[int] = None) -> None:
         self._labeled_correct = 0.0
         self._unlabeled_correct = 0.0
-        self._exact_labeled_correct = 0.0
-        self._exact_unlabeled_correct = 0.0
-        self._total_words = 0.0
-        self._total_sentences = 0.0
+
+        # we're interested in edges rather than words because a word can have multiple edges.
+        self._num_gold_edges = 0.0
+        self._num_pred_edges = 0.0
 
         self._ignore_classes: List[int] = ignore_classes or []
 
@@ -33,8 +38,10 @@ class EnhancedAttachmentScores(Metric):
         self,
         predicted_indices: torch.Tensor,
         predicted_labels: torch.Tensor,
+        predicted_labeled_arcs: torch.Tensor,
         gold_indices: torch.Tensor,
         gold_labels: torch.Tensor,
+        gold_labeled_arcs: torch.Tensor,
         mask: Optional[torch.Tensor] = None,
     ):
         """
@@ -42,6 +49,8 @@ class EnhancedAttachmentScores(Metric):
         predicted_indices : `torch.Tensor`, required.
             A tensor of head index predictions of shape (batch_size, timesteps).
         predicted_labels : `torch.Tensor`, required.
+            A tensor of arc label predictions of shape (batch_size, timesteps).
+        predicted_labeled_arcs : `torch.Tensor`, required.
             A tensor of arc label predictions of shape (batch_size, timesteps).
         gold_indices : `torch.Tensor`, required.
             A tensor of the same shape as `predicted_indices`.
@@ -55,62 +64,74 @@ class EnhancedAttachmentScores(Metric):
         )
         predicted_indices, predicted_labels, gold_indices, gold_labels, mask = unwrapped
 
-        mask = mask.long()
-        predicted_indices = predicted_indices.long()
-        predicted_labels = predicted_labels.long()
-        gold_indices = gold_indices.long()
-        gold_labels = gold_labels.long()
+        #print("this is a new batch")
+        
+        # first do UAS and LAS separately (TODO: do more efficiently)
+        
+        # UAS
+        for gold_edges, gold_edge_labels, predicted_edges, predicted_edge_labels in zip(gold_indices, gold_labels, predicted_indices, predicted_labels):
+            self._num_gold_edges += len(gold_edges)
+            self._num_pred_edges += len(predicted_edges)        
+            for predicted_edge in predicted_edges:
+                if predicted_edge in gold_edges:
+                    self._unlabeled_correct += 1.
+        
+        # LAS
+        for gold_labeled_edges, predicted_labeled_edges in zip(gold_labeled_arcs, predicted_labeled_arcs):
+            #print(gold_labeled_edges, "-->", predicted_labeled_edges)
+        
+            for predicted_labeled_edge in predicted_labeled_edges:
+                if predicted_labeled_edge in gold_labeled_edges:
+                    self._labeled_correct += 1.
+            
 
-        # Multiply by a mask denoting locations of
-        # gold labels which we should ignore.
-        for label in self._ignore_classes:
-            label_mask = gold_labels.eq(label)
-            mask = mask * (~label_mask).long()
-
-        correct_indices = predicted_indices.eq(gold_indices).long() * mask
-        unlabeled_exact_match = (correct_indices + (1 - mask)).prod(dim=-1)
-        correct_labels = predicted_labels.eq(gold_labels).long() * mask
-        correct_labels_and_indices = correct_indices * correct_labels
-        labeled_exact_match = (correct_labels_and_indices + (1 - mask)).prod(dim=-1)
-
-        self._unlabeled_correct += correct_indices.sum()
-        self._exact_unlabeled_correct += unlabeled_exact_match.sum()
-        self._labeled_correct += correct_labels_and_indices.sum()
-        self._exact_labeled_correct += labeled_exact_match.sum()
-        self._total_sentences += correct_indices.size(0)
-        self._total_words += correct_indices.numel() - (1 - mask).sum()
 
     def get_metric(self, reset: bool = False):
         """
         # Returns
         The accumulated metrics as a dictionary.
         """
-        unlabeled_attachment_score = 0.0
-        labeled_attachment_score = 0.0
-        unlabeled_exact_match = 0.0
-        labeled_exact_match = 0.0
-        if self._total_words > 0.0:
-            unlabeled_attachment_score = float(self._unlabeled_correct) / float(self._total_words)
-            labeled_attachment_score = float(self._labeled_correct) / float(self._total_words)
-        if self._total_sentences > 0:
-            unlabeled_exact_match = float(self._exact_unlabeled_correct) / float(
-                self._total_sentences
-            )
-            labeled_exact_match = float(self._exact_labeled_correct) / float(self._total_sentences)
+        #unlabeled_attachment_score = 0.0
+        #labeled_attachment_score = 0.0
+        
+        unlabeled_precision = 0.0
+        unlabeled_recall = 0.0
+        unlabeled_f1 = 0.0
+        
+        labeled_precision = 0.0
+        labeled_recall = 0.0
+        labeled_f1 = 0.0
+
+        
+        if self._num_gold_edges > 0.0:         
+            # precision
+            unlabeled_precision = self._unlabeled_correct / self._num_pred_edges
+            labeled_precision = self._labeled_correct / self._num_pred_edges
+            
+            # recall
+            unlabeled_recall = self._unlabeled_correct / self._num_gold_edges
+            labeled_recall = self._labeled_correct / self._num_gold_edges
+            
+            # f1
+            unlabeled_f1 = 2 * self._unlabeled_correct / (self._num_pred_edges + self._num_gold_edges)
+            labeled_f1 = 2 * self._labeled_correct / (self._num_pred_edges + self._num_gold_edges)
+   
         if reset:
             self.reset()
         return {
-            "UAS": unlabeled_attachment_score,
-            "LAS": labeled_attachment_score,
-            "UEM": unlabeled_exact_match,
-            "LEM": labeled_exact_match,
+#            "EUAS": unlabeled_attachment_score,
+#            "ELAS": labeled_attachment_score
+            "unlabeled_precision": unlabeled_precision,
+            "unlabeled_recall": unlabeled_recall,
+            "unlabeled_f1": unlabeled_f1,
+            "labeled_precision": labeled_precision,
+            "labeled_recall": labeled_recall,
+            "labeled_f1": labeled_f1
         }
 
     @overrides
     def reset(self):
         self._labeled_correct = 0.0
         self._unlabeled_correct = 0.0
-        self._exact_labeled_correct = 0.0
-        self._exact_unlabeled_correct = 0.0
-        self._total_words = 0.0
-        self._total_sentences = 0.0
+        self._num_gold_edges = 0.0
+        self._num_pred_edges = 0.0
