f4c6c775e635de6ea8198e5e53b30922b4e96287
jbar-ry <james.barry26@mail.dcu.ie>
2020-03-05 20:53:32 +0000

    update predict script and call eval script

--- scripts/predict.sh
+++ scripts/predict.sh
@@ -1,3 +1,19 @@
 #!/bin/bash
 
-allennlp predict /home/jbarry/allennlp-tutorial-practice/logs/e_parser_gpu/model.tar.gz data/UD_English-EWT/en_ewt-ud-dev_no_ellipsis.conllu --output-file x.conllu --predictor enhanced-predictor --include-package tagging --use-dataset-reader
+mkdir -p output
+
+GOLD="data/UD_English-EWT/en_ewt-ud-dev_no_ellipsis.conllu"
+PRED="output/en_ewt-ud-dev_no_ellipsis_pred.conllu"
+
+MODEL_DIR=logs/enhanced_parser_10_epochs_with_full_decoding
+
+#=== Predict ===
+allennlp predict ${MODEL_DIR}/model.tar.gz ${GOLD} \
+	--output-file ${PRED} \
+       	--predictor enhanced-predictor \
+	--include-package tagging \
+	--use-dataset-reader
+
+echo "Running UD Shared Task evaluation script..."
+python scripts/iwpt20_xud_eval.py --verbose $GOLD $PRED
+
