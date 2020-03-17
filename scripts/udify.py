af52bf4d0bbd4f2dc310495de3f8a683870a6248
Joachim Wagner <jowagner@users.noreply.github.com>
2020-03-17 19:11:01 +0000

    placeholder modules

---
+++ scripts/udify.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# (C) 2019 Dublin City University
+# All rights reserved. This material may not be
+# reproduced, displayed, modified or distributed without the express prior
+# written permission of the copyright holder.
+
+# Author: Joachim Wagner
+
+# For Python 2-3 compatible code
+# https://python-future.org/compatible_idioms.html
+
+from __future__ import print_function
+
+import os
+import subprocess
+import sys
+
+def supports(lcode):
+    return False
+
+def main():
+    raise NotImplementedError
+
+if __name__ == "__main__":
+    main()
+
---
+++ scripts/udpipe_polyglot.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# (C) 2020 Dublin City University
+# All rights reserved. This material may not be
+# reproduced, displayed, modified or distributed without the express prior
+# written permission of the copyright holder.
+
+# Author: Joachim Wagner
+
+# For Python 2-3 compatible code
+# https://python-future.org/compatible_idioms.html
+
+from __future__ import print_function
+
+import os
+import subprocess
+import sys
+
+import common_udpipe_future
+import utilities
+
+def supports(lcode):
+    return False
+
