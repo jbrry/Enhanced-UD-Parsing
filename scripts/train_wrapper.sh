4f99b2b05e1b3c058a5d51c0112926593826b952
jbar-ry <james.barry26@mail.dcu.ie>
2020-04-05 16:43:39 +0100

    fix loop

--- scripts/train_wrapper.sh
+++ scripts/train_wrapper.sh
@@ -11,36 +11,36 @@ test -z $2 && echo "Missing model type <dm> or <kg>"
 test -z $2 && exit 1
 MODEL_TYPE=$2
 
-test -z $3 && echo "Missing job group number <1:5> or <X> for specific job"
+test -z $3 && echo "Missing job group number <A:F>"
 test -z $3 && exit 1
 JOB=$3
 
 # job groups based on training data size distributed in a round-robin fashion.
 # run python utils/get_training_information.py to get training metadata.
-JOB_1="cs_pdt it_isdt lv_lvtb uk_iu ta_ttb"
-JOB_2="ru_syntagrus en_ewt bg_btb sv_talbanken"
-JOB_3="cs_cac nl_alpino sk_snk lt_alksnis"
-JOB_4="pl_pdb fi_tdt ar_padt fr_sequoia"
-JOB_5="pl_lfg cs_fictree nl_lassysmall et_ewt"
+JOB_A="cs_pdt it_isdt lv_lvtb uk_iu ta_ttb"
+JOB_B="ru_syntagrus en_ewt bg_btb sv_talbanken"
+JOB_C="cs_cac nl_alpino sk_snk lt_alksnis"
+JOB_D="pl_pdb fi_tdt ar_padt fr_sequoia"
+JOB_E="pl_lfg cs_fictree nl_lassysmall et_ewt"
 
 # append specific tbids here
-JOB_X="cs_cac cs_pdt it_isdt ru_syntagrus"
+JOB_F="ru_syntagrus"
 
-if [ $JOB == 1 ]; then
-  JOB_GROUP=$JOB_1
-elif [ $JOB == 2 ]; then
-  JOB_GROUP=$JOB_2
-elif [ $JOB == 3 ]; then
-  JOB_GROUP=$JOB_3
-elif [ $JOB == 4 ]; then
-  JOB_GROUP=$JOB_4
-elif [ $JOB == 5 ]; then
-  JOB_GROUP=$JOB_5
-elif [ $JOB == "X" ]; then
-  JOB_GROUP=$JOB_X
+if [ ${JOB} == "A" ]; then
+  JOB_GROUP=$JOB_A
+elif [ ${JOB} == "B" ]; then
+  JOB_GROUP=$JOB_B
+elif [ ${JOB} == "C" ]; then
+  JOB_GROUP=$JOB_C
+elif [ ${JOB} == "D" ]; then
+  JOB_GROUP=$JOB_D
+elif [ ${JOB} == "E" ]; then
+  JOB_GROUP=$JOB_E
+elif [ ${JOB} == "F" ]; then
+  JOB_GROUP=$JOB_F
 fi 
        
-for TBID in "${JOB_GROUP[*]}"; do
+for TBID in ${JOB_GROUP[@]}; do
     echo "training on ${TBID}"
 
     bash scripts/train.sh ${TASK} ${MODEL_TYPE} ${TBID}
