401c0fea3a190ae9bc19fbfdbd3d199971b247fd
Joachim Wagner <jwagner+grove2018@computing.dcu.ie>
2020-04-14 10:58:11 +0100

    do not repurpose standard shell variable LANG; add example usage

--- scripts/run-convert-fasttext-to-npz.sh
+++ scripts/run-convert-fasttext-to-npz.sh
@@ -7,6 +7,8 @@
 #SBATCH --ntasks=1
 #SBATCH -N 1-1
 
+# example: sbatch --export=ALL,FT_LCODE=ru run-convert-fasttext-to-npz.sh
+
 CONLL17_DIR=${HOME}/data/2-conll17
 TMP_DIR=${CONLL17_DIR}/tmp
 test -z ${PRJ_DIR} && PRJ_DIR=${HOME}/enhanced-ud/Enhanced-UD-Parsing
@@ -16,22 +18,19 @@ TEXT_DIR=${CONLL17_DIR}/text
 
 source ${PRJ_DIR}/../UDPipe-Future/venv-udpf/bin/activate
 
-test -z $LCODE && echo "Missing LCODE"
-test -z $LCODE && exit 1
+test -z $FT_LCODE && echo "Missing LCODE"
+test -z $FT_LCODE && exit 1
 
-test -z $LANG && echo "Missing LANG"
-test -z $LANG && exit 1
-
-echo "== $LCODE $LANG =="
+echo "== $FT_LCODE =="
 date
 
 cd ${TEXT_DIR}
-hostname > convert_$LCODE.start
+hostname > convert_$FT_LCODE.start
 
 python3 ${PRJ_DIR}/../UDPipe-Future/embeddings/vec_to_npz/convert.py \
-    --max_words 1000000 model_$LCODE.vec fasttext-$LCODE.npz
+    --max_words 1000000 model_$FT_LCODE.vec fasttext-$FT_LCODE.npz
 
-touch convert_$LCODE.end
+touch convert_$FT_LCODE.end
 
 echo "Finished"
 date
--- scripts/run-train-fasttext.sh
+++ scripts/run-train-fasttext.sh
@@ -2,9 +2,11 @@
   
 #SBATCH -p compute       # which partition to run on
 #SBATCH -J fasttext   # name for the job
-#SBATCH --mem=128000
+#SBATCH --mem=161099
 #SBATCH --exclusive      # TODO: our compiled version seems to only use 12 cores
 #SBATCH --constraint=avx
+
+# example: sbatch --export=ALL,FT_LANG=Russian,FT_LCODE=ru run-train-fasttext.sh
 
 CONLL17_DIR=${HOME}/data/2-conll17
 TMP_DIR=${CONLL17_DIR}/tmp
@@ -13,19 +15,20 @@ test -z ${PRJ_DIR} && PRJ_DIR=${HOME}/enhanced-ud/Enhanced-UD-Parsing
 SCRIPT_DIR=${PRJ_DIR}/scripts
 TEXT_DIR=${CONLL17_DIR}/text
 
-test -z $LCODE && echo "Missing LCODE"
-test -z $LCODE && exit 1
+test -z $FT_LCODE && echo "Missing LCODE"
+test -z $FT_LCODE && exit 1
 
-test -z $LANG && echo "Missing LANG"
-test -z $LANG && exit 1
+test -z $FT_LANG && echo "Missing LANG"
+test -z $FT_LANG && exit 1
 
-echo "== $LCODE $LANG =="
+echo "== $FT_LCODE $FT_LANG =="
 date
+hostname
 
 cd ${TEXT_DIR}
-hostname > model_$LCODE.start
-fasttext skipgram -minCount 5 -epoch 10 -neg 10 -input $LANG.txt -output model_$LCODE
-touch model_$LCODE.end
+hostname > model_$FT_LCODE.start
+fasttext skipgram -minCount 5 -epoch 10 -neg 10 -input $FT_LANG.txt -output model_$FT_LCODE
+touch model_$FT_LCODE.end
 
 echo "Finished"
 date
