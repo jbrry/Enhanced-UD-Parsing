e33431b7a53898362de77ab996fd6db80aa6a21c
Joachim Wagner <jowagner@users.noreply.github.com>
2020-04-24 19:38:23 +0100

    bug fix: typo causing mark-cc-rel to be skipped

--- scripts/create_copy2e_module_symlinks.sh
+++ scripts/create_copy2e_module_symlinks.sh
@@ -8,7 +8,7 @@
 SOURCE=copy_parse.py
 
 for EXTRA in     _rel             _cc             _cc_rel \
-    _mark        _mark_rel        _mark_cc        _marc_cc_rel \
+    _mark        _mark_rel        _mark_cc        _mark_cc_rel \
     _encase      _encase_rel      _encase_cc      _encase_cc_rel \
     _encase_mark _encase_mark_rel _encase_mark_cc _encase_mark_cc_rel \
     _arcase      _arcase_rel      _arcase_cc      _arcase_cc_rel \
