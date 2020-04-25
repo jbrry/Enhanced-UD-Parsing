dd9695dac7f8d3c98a73785afaa1eea736bd4d72
jbar-ry <james.barry26@mail.dcu.ie>
2020-04-25 10:43:15 +0100

    add script to run quick fix

---
+++ scripts/fix_all_final.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+# usage: ./scripts/fix_all_final.sh tmp/final_tmp/ tmp/final_fixed/
+
+# directory where you have copied the test files to
+TMP_DIR=$1
+
+# where to write fixed output
+FIXED_DIR=$2
+
+echo "searching ${TMP_DIR}"
+
+for file in $(ls $TMP_DIR); do 
+    echo "found $file"
+    
+    LCODE=$(echo ${file} | awk -F "_" '{print $2}')
+    echo "using $LCODE"
+
+    # adjust path to tools if necessary
+    perl ${HOME}/tools/conllu-quick-fix.pl < $TMP_DIR/$file > $FIXED_DIR/$file.conllu
+
+done 
+
