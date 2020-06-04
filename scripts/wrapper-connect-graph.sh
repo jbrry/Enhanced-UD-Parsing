06601f24b1221d8e801595d203a72e80ad1e8a39
Joachim Wagner <jowagner@users.noreply.github.com>
2020-06-04 17:07:54 +0100

    comment on how to activate `best-guess` mode

--- scripts/wrapper-connect-graph.sh
+++ scripts/wrapper-connect-graph.sh
@@ -8,6 +8,9 @@ TEMP_DIR=$(mktemp -d eud-fragment-fix.XXXXXX)
 
 python3 $SCRIPT_DIR/connect_graph.py -i /dev/stdin -o $TEMP_DIR
 
+## future work: best_guess mode (only tiny improvements so far)
+#python3 $SCRIPT_DIR/connect_graph.py --mode best_guess -i /dev/stdin -o $TEMP_DIR
+
 cat $TEMP_DIR/*
 
 rm -rf $TEMP_DIR
